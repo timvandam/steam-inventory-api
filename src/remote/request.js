2a853f175399a82da7f5cdbbe7354cd798ac11a1
Tim van Dam <timvandamcs@gmail.com>
2020-04-02 20:56:08 +0200

    another error handling fix o_o

--- src/remote/request.js
+++ src/remote/request.js
@@ -10,11 +10,11 @@ const server = new Server(parseInt(process.env.REQUESTER_PORT))
 server.module.exports = {
   get (url) {
     return axios.get(url)
+      .catch(error => Promise.reject(error.toJSON()))
       .then(response => {
         if (response.status !== 200) return Promise.reject(new Error('Non-200 status code'))
         const { data } = response
         return data
       })
-      .catch(error => Promise.reject(error.toJSON()))
   }
 }
