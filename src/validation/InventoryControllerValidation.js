84a191718dc9e0d913f156b8643b96f6c6c81453
Tim van Dam <timvandamcs@gmail.com>
2020-04-02 20:51:01 +0200

    fixed even more error messages

--- src/validation/InventoryControllerValidation.js
+++ src/validation/InventoryControllerValidation.js
@@ -6,11 +6,11 @@
  */
 function validateGetInventory (req, res, next) {
   const { steamId, appId, contextId } = req.params
-  if (!steamId) return req.status(400).json({ error: new Error('No steamid was provided') })
-  if (!appId) return req.status(400).json({ error: new Error('No appid was provided') })
-  if (!contextId) return req.status(400).json({ error: new Error('No contextid was provided') })
-  if (isNaN(appId)) return req.status(400).json({ error: new Error('Appid must be a number') })
-  if (isNaN(contextId)) return req.status(400).json({ error: new Error('Contextid must be a number') })
+  if (!steamId) return req.status(400).json({ error: 'No steamid was provided' })
+  if (!appId) return req.status(400).json({ error: 'No appid was provided' })
+  if (!contextId) return req.status(400).json({ error: 'No contextid was provided' })
+  if (isNaN(appId)) return req.status(400).json({ error: 'Appid must be a number' })
+  if (isNaN(contextId)) return req.status(400).json({ error: 'Contextid must be a number' })
   req.params.appId = parseInt(appId)
   next()
 }
